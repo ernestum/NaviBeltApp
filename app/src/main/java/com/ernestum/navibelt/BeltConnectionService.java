ae62ae3f9e76c91494234a5aab6b71fac413f4fa
Maximilian Ernestus <maximilian@ernestus.de>
2021-08-18 16:29:41 +0200

    Minimal working example to enable a LED together with the CallbackLED example flashed on the nano.

--- app/src/main/java/com/ernestum/navibelt/BeltConnectionService.java
+++ app/src/main/java/com/ernestum/navibelt/BeltConnectionService.java
@@ -3,7 +3,10 @@ package com.ernestum.navibelt;
 import android.Manifest;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanFilter;
@@ -21,12 +24,16 @@ import android.os.ParcelUuid;
 import android.util.Log;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 public class BeltConnectionService extends Service {
+    private final UUID service_uuid = UUID.fromString("19B10000-E8F2-537E-4F6C-D104768A1214");
+    private final UUID direction_characteristic_uuid = UUID.fromString("19B10001-E8F2-537E-4F6C-D104768A1214");
     public BeltConnectionService() {
     }
 
     BluetoothAdapter bluetoothAdapter;
+    BluetoothGatt gatt;
 
     @Override
     public void onCreate() {
@@ -34,8 +41,7 @@ public class BeltConnectionService extends Service {
         requestBluetoothEnableIfDisabled();
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(bluetoothAdapter.isEnabled()) {
-            startScanningForBelt();
-//            connectToBelt();
+            startScanForBelt();
         }
 
         registerReceiver(deviceStateChangeReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
@@ -64,10 +70,49 @@ public class BeltConnectionService extends Service {
             super.onScanResult(callbackType, result);
 
             Log.d("ConnectionService", "Found device! " + result.toString());
+            gatt = result.getDevice().connectGatt(BeltConnectionService.this.getBaseContext(), false, new BluetoothGattCallback() {
+
+                @Override
+                public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+                    super.onConnectionStateChange(gatt, status, newState);
+                    gatt.discoverServices();
+                    Log.d("BLE", "gatt connected!");
+                    // TODO: notify of connection loss if we disconnected
+                }
+
+                @Override
+                public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+                    super.onServicesDiscovered(gatt, status);
+                    Log.d("BLE", "Services discovered!");
+                    BluetoothGattService service = gatt.getService(service_uuid);
+                    BluetoothGattCharacteristic characteristic = service.getCharacteristic(direction_characteristic_uuid);
+                    characteristic.setValue(new byte[] {1});
+                    gatt.writeCharacteristic(characteristic);
+                    Log.d("BLE", "Characeristic written!");
+                    // TODO: notify that we are now connected!
+                }
+
+                @Override
+                public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+                    super.onCharacteristicRead(gatt, characteristic, status);
+                    // TODO: notify of new angle if it is the current angle characteristic
+                }
+
+
+                @Override
+                public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                    super.onCharacteristicChanged(gatt, characteristic);
+                    // TODO: notify of new angle if it is the current angle characteristic
+                }
+
+            });
+
+
+            scanner.stopScan(scanCallback);
         }
     };
 
-    private void startScanningForBelt() {
+    private void startScanForBelt() {
 
         if(!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
             Log.d("belt", "got no permission!");
@@ -77,11 +122,9 @@ public class BeltConnectionService extends Service {
 
         scanner = bluetoothAdapter.getBluetoothLeScanner();
 
-//        ScanSettings scanSettings = new ScanSettings.Builder().build();
-//        ScanFilter serviceFilter = new ScanFilter.Builder().setServiceUuid(ParcelUuid.fromString("22d40000-458f-44cc-b199-2d6ae6c69984")).build();
-
-//        scanner.startScan(Arrays.asList(serviceFilter) , scanSettings, scanCallback);
-        scanner.startScan(scanCallback);
+        ScanSettings scanSettings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build();
+        ScanFilter serviceFilter = new ScanFilter.Builder().setServiceUuid(new ParcelUuid(service_uuid)).build();
+        scanner.startScan(Arrays.asList(serviceFilter) , scanSettings, scanCallback);
         Log.d("ConnectionService","Started Scan!");
 
         new Handler().postDelayed(new Runnable() {
@@ -91,20 +134,7 @@ public class BeltConnectionService extends Service {
                 Log.d("ConnectionService", "Scanning stopped after 30s!");
             }
         }, 30000);
-
     }
-
-    private void connectToBelt() {
-        try {
-            BluetoothDevice device = bluetoothAdapter.getRemoteDevice("7e:22:eb:93:f6:06");
-        } catch (IllegalArgumentException exception) {
-            Log.d("ConnectionService", "Device not found with provided address.");
-            return;
-        }
-        Log.d("ConnectionService", "Connected to belt!");
-
-    }
-
 
     public class LocalBinder extends Binder {
         BeltConnectionService getService() {
@@ -125,19 +155,18 @@ public class BeltConnectionService extends Service {
                 switch (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)) {
                     case BluetoothAdapter.STATE_OFF:
                         // Bluetooth has been turned off;
-                        // TODO: handle this
+                        // TODO: notify user that we disconnected
                         break;
                     case BluetoothAdapter.STATE_TURNING_OFF:
                         // Bluetooth is turning off;
+                        // TODO: disable all motors for graceful shutdown
                         break;
                     case BluetoothAdapter.STATE_ON:
                         // Bluetooth is on
-                        startScanningForBelt();
-//                        connectToBelt();
+                        startScanForBelt();
                         break;
                     case BluetoothAdapter.STATE_TURNING_ON:
                         // Bluetooth is turning on
-                        // TODO: disable all motors for graceful shutdown
                         break;
                 }
             }
